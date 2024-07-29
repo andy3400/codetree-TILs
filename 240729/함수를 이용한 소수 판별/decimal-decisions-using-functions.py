11c34d77ad7dad35d29a76ad4c7261808a25714d
lee taeho <51397856+andy3400@users.noreply.github.com>
2024-07-29 12:58:13 +0900

    Title: 함수를 이용한 소수 판별, Time: 118ms, Memory: 26MB, Status: Passed - CodeTree

--- 240729/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240729/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,8 +1,9 @@
 def cal_prime(n):
+    if n <=1:
+        return 0
+        
     for i in range(2, int(n**0.5)+1):
         #  나누어 떨어진다면
-        if n <=1:
-            return 0
         if n%i == 0:
             return 0
     return 1
