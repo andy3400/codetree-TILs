96b9b71c31984c2d2c1b42dd423afe43e106dec5
lee taeho <51397856+andy3400@users.noreply.github.com>
2024-07-29 13:02:53 +0900

    Title: 함수를 이용한 윤년 판별, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,16 @@
+def cal_yune(n):
+    if n % 4 == 0:
+        if n%100 == 0 and n% 400 != 0:
+            return "false"
+        return "true"
+
+
+
+def main():
+    n = int(input())
+    print(cal_yune(n))
+
+
+
+if __name__=="__main__":
+    main()
