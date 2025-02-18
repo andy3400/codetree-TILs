9de65b649a2cf9f33051a558519b29e683b9d31a
andy <51397856+andy3400@users.noreply.github.com>
2025-02-18 17:23:07 +0900

    Title: 나누고 빼면서 합하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250218/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 250218/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -2,20 +2,20 @@ n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
 sum_list = []
-result = 0
+result = 0  # 초기 result는 0으로 설정
 
-
+# m이 0보다 클 때까지 반복
 while m > 0:
-    if m % 2 == 0:
-        sum_list.append(m//2)
-        m//=2
-    else:
-        sum_list.appedn(m-1)
-        m = (m-1) // 2
+    if m % 2 == 0:  # m이 짝수일 때
+        sum_list.append(m // 2)
+        m //= 2
+    else:  # m이 홀수일 때
+        sum_list.append(m - 1)
+        m = (m - 1) // 2
 
-
+# sum_list에서 계산한 인덱스를 통해 결과를 구함
 for index in sum_list:
-    if 0 <= index < len(A):
+    if 0 <= index < len(A):  # A 배열의 인덱스 범위 내에서만 접근
         result += A[index]
 
-print(result)
+print(result-1)
