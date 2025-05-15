d11662c337ac293578e3b6a48a3718bbf1b60db9
andy <51397856+andy3400@users.noreply.github.com>
2025-05-15 17:17:22 +0900

    Title: A 부터 B 까지, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250515/A 부터 B 까지/a-to-b.py
+++ 250515/A 부터 B 까지/a-to-b.py
@@ -4,7 +4,12 @@ print(A, end= " ")
 while A < B:
     if A%2!=0:
         A*=2
+        if A > B:
+            break
         print(A, end= " ")
     elif A%2==0:
         A+=3
+        if A > B:
+            break
         print(A, end= " ")
+        
